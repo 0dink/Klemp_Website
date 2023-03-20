125133d3ad0a8a0ac845097c889ac15cc28c6522
Odin K <90708114+0dink@users.noreply.github.com>
2023-03-20 17:23:17 -0400

    updated ERC and CAMP

---
+++ campus_tour/buildings/CAMP/CAMP.js
@@ -0,0 +1,10 @@
+function searchbar() {
+    //var x = document.getElementById('searchTXT').value
+    //document.get
+    document.getElementById("popupSearch").style.display = "block";
+    //window.alert(x)
+    
+}
+function closeForm() {
+  document.getElementById("popupSearch").style.display = "none";
+}
