1eddb24522a7f2506616bfc52154256c714fc993
Megan <edwardmm@Clarkson.edu>
2023-04-01 14:08:10 -0400

    temp rowley changes

--- campus_tour/MainView.js
+++ campus_tour/MainView.js
@@ -1,11 +1,13 @@
 function importData() {
-    let input = document.createElement('input');
-    input.type = 'file';
-    input.onchange = _ => {
-      // you can use this method to get file and perform respective operations
-              let files =   Array.from(input.files);
-              console.log(files);
-          };
-    input.click();
-    
-  }
+  let input = document.createElement('input');
+  input.type = 'file';
+  input.onchange = _ => {
+    // you can use this method to get file and perform respective operations
+            let files =   Array.from(input.files);
+            console.log(files);
+        };
+  input.click();
+  
+}
+
+
