0f5995c6ff59155ed18c2a2f41af9abfbdf1f62f
Odin K <90708114+0dink@users.noreply.github.com>
2023-04-08 18:26:35 -0400

    fixed CAMP to run locally

---
+++ campus_tour/CSV_files/CAMP_onload.js
@@ -0,0 +1,96 @@
+function CAMP1_CSV_to_Array() {
+    var xhr = new XMLHttpRequest();// Create a new XMLHttpRequest object
+    
+    xhr.onload = function() {// Set the onload callback function
+      if (xhr.status === 200) {
+        var lines = xhr.responseText.split('\n');// Parse the CSV data
+        var dataArray = [];
+        for (var i = 0; i < lines.length; i++) {
+          var values = lines[i].split(',');
+          var obj = {
+            RoomName: values[0],
+            XCoord: values[1],
+            YCoord: values[2],
+            Path: values[3],
+            Display: values[4]
+          };
+          dataArray.push(obj);
+        }
+        
+        localStorage.setItem('CAMP1_Array', JSON.stringify(dataArray));// Store the array in localStorage
+
+        console.log('First element:', dataArray[1]);
+        console.log('First element:', dataArray[2]);
+        console.log('First element:', dataArray[3]);
+      }
+    };
+    
+    var file = './campus_tour/CSV_files/CAMP_first_floor_table.csv';  // Set the default file path
+
+    xhr.open('GET', file, true);// Open the file and send the request
+    xhr.send();
+  }
+  function CAMP2_CSV_to_Array() {
+    var xhr = new XMLHttpRequest();// Create a new XMLHttpRequest object
+    
+    xhr.onload = function() {// Set the onload callback function
+      if (xhr.status === 200) {
+        var lines = xhr.responseText.split('\n');// Parse the CSV data
+        var dataArray = [];
+        for (var i = 0; i < lines.length; i++) {
+          var values = lines[i].split(',');
+          var obj = {
+            RoomName: values[0],
+            XCoord: values[1],
+            YCoord: values[2],
+            Path: values[3],
+            Display: values[4]
+          };
+          dataArray.push(obj);
+        }
+        
+        localStorage.setItem('CAMP2_Array', JSON.stringify(dataArray));// Store the array in localStorage
+
+        console.log('First element:', dataArray[1]);
+        console.log('First element:', dataArray[2]);
+        console.log('First element:', dataArray[3]);
+      }
+    };
+    
+    var file = './campus_tour/CSV_files/CAMP_second_floor_table.csv';  // Set the default file path
+
+    xhr.open('GET', file, true);// Open the file and send the request
+    xhr.send();
+  }
+  function CAMP3_CSV_to_Array() {
+    var xhr = new XMLHttpRequest();// Create a new XMLHttpRequest object
+    
+    xhr.onload = function() {// Set the onload callback function
+      if (xhr.status === 200) {
+        var lines = xhr.responseText.split('\n');// Parse the CSV data
+        var dataArray = [];
+        for (var i = 0; i < lines.length; i++) {
+          var values = lines[i].split(',');
+          var obj = {
+            RoomName: values[0],
+            XCoord: values[1],
+            YCoord: values[2],
+            Path: values[3],
+            Display: values[4]
+          };
+          dataArray.push(obj);
+        }
+        
+        localStorage.setItem('CAMP3_Array', JSON.stringify(dataArray));// Store the array in localStorage
+
+        console.log('First element:', dataArray[1]);
+        console.log('First element:', dataArray[2]);
+        console.log('First element:', dataArray[3]);
+      }
+    };
+    
+    var file = './campus_tour/CSV_files/CAMP_third_floor_table.csv';  // Set the default file path
+
+    xhr.open('GET', file, true);// Open the file and send the request
+    xhr.send();
+  }
--- campus_tour/buildings/CAMP/README.txt
+++ campus_tour/buildings/CAMP/README.txt
@@ -1,7 +1,63 @@
-the changes to main changes to the HTML file are as follows please look at line 35 - 39 on CAMP_two for example 
-also add this <script src="https://cdn.jsdelivr.net/npm/papaparse@5.3.0"></script> under the head tag
-also see changes on line 19 in the body tag
-for changes in the css please see the discord to-do channel
-changes in the JS script should be self explanator if you have trouble feel free to ask me
+to see an example of changes to the goto function see lines 13, 16-26, 82-87 
+in CAMP.js
 
-to test if the changes work go into respective CSV file and change false to true for the first room and a black square should apear when you load page
+IMPORTANT 
+you will be copy and pasting the goto function in CAMP.js to the js
+files of your buildings
+
+IMPORTANT 
+make note of the names that the goto function uses those are the names
+you will need to use 
+
+for example if you had CAMP as your building you would need to write 
+
+filename = JSON.parse(localStorage.getItem('CAMP1_Array'));
+saveto = 'CAMP1_Array';
+
+to see example of changes to searchandCreate see lines 132-205 in CAMP.js
+
+to see example of changes to html file see please see line 23 in CAMP_one.html
+
+please create a onload.js for each of your floor
+for an example please see CAMP_onload.js under CSV_files
+
+please makesure that your functions in your onloand.js are called in 
+homepage.html please see lines 8 abd 21 in homepage.html
+
+
+IMPORTANT 
+make note of the names that the goto function uses those are the names
+you will need to use in your onload functions 
+
+for example if you had CAMP as your building you would need to write your
+onload function to create a array named CAMP1_Array 
+
+filename = JSON.parse(localStorage.getItem('CAMP1_Array'));
+saveto = 'CAMP1_Array';
+
+
+TESTING 
+you need to launch from the hompage or else it wont work
+
+to test between pages change the some of the links in the CSV like this 
+"https://webspace.clarkson.edu/projects/KLEMP/public_html/Klemp_Website-main/campus_tour/buildings/CAMP/CAMP_one.html"
+to this "http://127.0.0.1:5500/campus_tour/buildings/CAMP/CAMP_one.html"
+
+or whatever the ip you are hosting the website from when you use liveView
+
+also all of CAMP_3 and ERC_2 link are written wrong so dont bother trying 
+to search for rooms on those floor because it wont work 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
