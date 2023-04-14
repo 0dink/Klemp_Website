9172ffd965f9e4de276c8d48b7b0329abc9cf944
Megan <edwardmm@Clarkson.edu>
2023-04-14 14:31:20 -0400

    Final Upload Schedule Edits + Reset Button Added

--- campus_tour/MainView.js
+++ campus_tour/MainView.js
@@ -47,15 +47,104 @@ function dispLocation(locations) {
     }
 }
 
+function changeSpan(locations,names)
+{
+  //array lengths
+  var locationLength = locations.length;
 
+  //vars from html code
+  var snell = document.getElementById("snellSpan");
+  var sc = document.getElementById("scSpan");
+  var camp = document.getElementById("campSpan");
+  var rowley = document.getElementById("roSpan");
+  var cheel = document.getElementById("cheelSpan");
+  //var quad = docuement.getElementById("quadSpan");
+  var price = document.getElementById("priceSpan");
+
+  //vars for new spans
+  var snellList = 'none';
+  var scList = 'none';
+  var campList = 'none';
+  var roList = 'none';
+  var cheelList = 'none';
+  //var quadList = 'none';
+  var priceList = 'none';
+
+  //get new text
+  for(i=0; i<locationLength; i++)
+  {
+     let curBuilding = locations[i];
+     
+     if (curBuilding.includes("Bertrand"))
+     {
+       snellList += names[i] + ' ';
+     }
+     if(curBuilding.includes("Science"))
+     {
+       scList += names[i] + ' ';
+     }
+     if(curBuilding.includes("CAMP"))
+     {
+      campList += names[i] + ' ';
+     }
+     if(curBuilding.includes("Rowley"))
+     {
+      roList += names[i] + ' ';
+     }
+     if(curBuilding.includes("Cheel"))
+     {
+      cheelList += names[i] + ' ';
+     }
+     /*if(curBuilding.includes("Quad"))
+     {
+      quadList += names[i] + ' ';
+     }*/
+     if(curBuilding.includes("Price"))
+     {
+      priceList += names[i] + ' ';
+     }
+  }
+
+  //set new text only if needed
+  if(snellList != "none")
+  {
+    snell.innerText = snellList.slice(4);
+  }
+  if(scList != "none")
+  {
+    sc.innerText = scList.slice(4);
+  }
+  if(campList != "none")
+  {
+    camp.innerText =campList.slice(4);
+  }
+  if(roList != "none")
+  {
+    rowley.innerText = roList.slice(4);
+  }
+  if(cheelList != "none")
+  {
+    cheel.innerText = cheelList.slice(4);
+  }
+  /*if(quadList != "none")
+  {
+    quad.innerText = quadList.slice(4);
+  }*/
+  if(priceList != "none")
+  {
+    price.innerText = priceList.slice(4);
+  }
+  
+}
 
 function dispFile(contents)
 {
   let numClasses = count(contents,"DESCRIPTION:");
-  let text = "<ul id= 'schedule'>";
+  let locationList = [];
+  let nameList = [];
   var locations;
 
-  for(i =1; i <= numClasses; i++)
+  for(i =0; i < numClasses; i++)
   {
     classNamePos = contents.search("DESCRIPTION:") + 12;
     endClassNamePos = contents.search("FREQ=WEEKLY;")-6;
@@ -68,14 +157,13 @@ function dispFile(contents)
     console.log(locationPos + ' ' + endLocationPos + ' ' + classLocation);
 
     contents = contents.slice(endLocationPos + 3);
-    text += "<li>" + className + ' ' + classLocation +"</li>";
+    locationList[i] = classLocation;
+    nameList[i] = className;
     locations += classLocation + ' ';
   }
-
-  text += "</ul>";
  
-  document.getElementById('contents').innerHTML= text;
   dispLocation(locations);
+  changeSpan(locationList,nameList);
 }
 
 function clickElem(elem)
@@ -132,4 +220,42 @@ function searchbar() {
 }
 function closeForm() {
   document.getElementById("popupSearch").style.display = "none";
+}
+
+function resetMap()
+{
+  //resets all the pins not not display
+  let snellImg = document.getElementById("snellPin");
+  let scImg = document.getElementById("scPin");
+  let campImg = document.getElementById("campPin");
+  let roImg = document.getElementById("rowleyPin");
+  let cheelImg = document.getElementById("cheelPin");
+  let quadImg = document.getElementById("quadPin");
+  let priceImg = document.getElementById("pricePin");
+ 
+  snellImg.style.display = 'none';
+  scImg.style.display = 'none';
+  campImg.style.display = 'none';
+  roImg.style.display = 'none';
+  cheelImg.style.display = 'none';
+  quadImg.style.display = 'none';
+  priceImg.style.display = 'none';
+
+  //resets the spans to original text
+  var snellSpan = document.getElementById("snellSpan");
+  var scSpan = document.getElementById("scSpan");
+  var campSpan = document.getElementById("campSpan");
+  var rowleySpan = document.getElementById("roSpan");
+  var cheelSpan = document.getElementById("cheelSpan");
+  //var quadSpan = docuement.getElementById("quadSpan");
+  var priceSpan = document.getElementById("priceSpan");
+
+  snellSpan.innerText = "Bertrand H. Snell Hall is home to the David D.Reh School of Business,the Department of Communication, Media, and Design, the Department of Humanities and Social Studies and our Engineering and Management program.This building also contains collaborative learning environments including a design lab, computer lab, an art studio, and a teaching lab.";
+  scSpan.innerText = "The Cora and Bayard Clarkson Science Center houses many departments including biology, biomolecular science, chemistry, computer science, data science, mathematics, physics, and psycology. The President's Office is also located in the Science Center in room 321 and can be contacted at president@clarkson.edu or (315)268-6444. Concrete Cafe, a grab and go dining location, can be found on the third floor.";
+  campSpan.innerText = "The Center for Advanced Materials Processing is home to Clarkson's Wallace H. Coulter School of Engineering which contains all of our enginnering programs. CAMP also is a research center in material proccesing, synthesis, functionalization, and characterization.In additon, Clarkson's 12 SPEED teams are also housed here, giving student the opportunity to work in collaborative project teams and compete with them.";
+  rowleySpan.innerText = "Rowly Labs is connected to CAMP and houses many computer and research labs. Many professors offices in the enginnering department can be found in Rowley.";
+  cheelSpan.innerText = "Cheel Campus Center is the current home ice for Clarkson men's and women's Division 1 hockey teams, where students can get free admission to games. Cheel also contanains Yianoukos Fitness Center which is a newly rennovated fitness center including a climbing wall.There is also a grab and go dining option available at the Disefano's Family Cafe housed in Cheel. ";
+  //quadSpan.innerText = Quad's origianl tooltip
+  priceSpan.innerText = "Price Hall is made of four wings: Farrisee, Thomas, Newell, and Ormsby. Each wing can house 60 mainly upperclass students in two bedroom suites housing four students. Price Hall also contains the Clarkson School and the Honors Office which are housed in the Newell and Ormsby wings.";
+
 }
