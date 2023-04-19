1ae3c7b765cf44980fe536513f4788110c014055
NameNotGiven <58343843+NameNotGiven@users.noreply.github.com>
2023-04-19 15:12:43 -0400

    fixed sciencecenter.js

--- campus_tour/buildings/science center/ScienceCenter.js
+++ campus_tour/buildings/science center/ScienceCenter.js
@@ -6,140 +6,131 @@ function searchbar() {
 }
 
 function goTo(){
-    var prefix = roomName.slice(0, 2); // extract the first two characters of the search value
-    var floor = roomName.slice(2, 3);
-    var realRoomName = roomName.substring(2);
-    var filename;
-    var saveto;
-    if (prefix === "CA") {
-        if (floor === '1') {
-            //filename = "../../CSV_files/CAMP_first_floor_table.csv";
-             filename = JSON.parse(localStorage.getItem('CAMP1_Array'));
-             saveto = 'CAMP1_Array';
-        } else if (floor === '2') {
-            //filename = "../../CSV_files/CAMP_second_floor_table.csv";
-            filename = JSON.parse(localStorage.getItem('CAMP2_Array'));
-            saveto = 'CAMP2_Array';
-        } else if (floor === '3') {
-            //filename = "../../CSV_files/CAMP_third_floor_table.csv";
-            filename = JSON.parse(localStorage.getItem('CAMP3_Array'));
-            saveto = 'CAMP3_Array'
-        } else{
-            return null;
-        }
-    } else if (prefix === "SC") {
-        if (floor === '1') {
-            //filename = "../../CSV_files/ScienceCenter_first_floor_table.csv";
-            filename = JSON.parse(localStorage.getItem('SC1_Array'));
-            saveto = 'SC1_Array';
-        } else if (floor === '2') {
-            filename = JSON.parse(localStorage.getItem('SC2_Array'));
-            saveto = 'SC2_Array';
-            //filename = "../../CSV_files/ScienceCenter_second_floor_table.csv";
-        } else if (floor === '3') {
-            filename = JSON.parse(localStorage.getItem('SC3_Array'));
-            saveto = 'SC3_Array';
-            //filename = "../../CSV_files/ScienceCenter_third_floor_table.csv";
-        } else {
-            return null;
-        }
-    } else if (prefix === "SN") {
-        if (floor === 'B') {
-            filename = JSON.parse(localStorage.getItem('SNB_Array'));
-            saveto = 'SNB_Array';
-            //filename = "../../CSV_files/Snell_zero_floor_table.csv";
-        } else if (floor === '1') {
-          filename = JSON.parse(localStorage.getItem('SN1_Array'));
-          saveto = 'SN1_Array';
-            //filename = "../../CSV_files/Snell_first_floor_table.csv";
-        } else if (floor === '2') {
-          filename = JSON.parse(localStorage.getItem('SN2_Array'));
-          saveto = 'SN2_Array';
-            //filename = "../../CSV_files/Snell_second_floor_table.csv";
-        } else if (floor === '3') {
-          filename = JSON.parse(localStorage.getItem('SN3_Array'));
-          saveto = 'SN3_Array';
-            //filename = "../../CSV_files/Snell_third_floor_table.csv";
-        } else {
-            return null;
-        }
-    } else if (prefix === "ST") {
-        if (floor === 'B') {
-          filename = JSON.parse(localStorage.getItem('STB_Array'));
-          saveto = 'STB_Array';
-            //filename = "../../CSV_files/StudentCenter_zero_floor_table.csv";
-        } else if (floor === '1') {
-          filename = JSON.parse(localStorage.getItem('ST1_Array'));
-          saveto = 'ST1_Array';
-            //filename = "../../CSV_files/StudentCenter_first_floor_table.csv";
-        } else if (floor === '2') {
-          filename = JSON.parse(localStorage.getItem('ST2_Array'));
-          saveto = 'ST2_Array';
-            //filename = "../../CSV_files/StudentCenter_second_floor_table.csv";
-        } else {
-            return null;
-        }
-    } else if (prefix === "ER") {
-        if (floor === '1') {
-          filename = JSON.parse(localStorage.getItem('ER1_Array'));
-          saveto = 'ER1_Array';
-            //filename = "../../CSV_files/ERC_first_floor_table.csv";
-        } else if (floor === '2') {
-          filename = JSON.parse(localStorage.getItem('ER2_Array'));
-          saveto = 'ER2_Array';
-            //filename = "../../CSV_files/ERC_second_floor_table.csv";
-        } else {
-            return null;
-        }
-    } else if (prefix === "RO") {
-        if (floor === '1') {
-          filename = JSON.parse(localStorage.getItem('RO1_Array'));
-          saveto = 'RO1_Array';
-            //filename = "../../CSV_files/Rowley_first_floor_table.csv";
-        } else if (floor === '2') {
-          filename = JSON.parse(localStorage.getItem('RO2_Array'));
-          saveto = 'RO2_Array';
-            //filename = "../../CSV_files/Rowley_second_floor_table.csv";
-        } else {
-            return null;
-        }
-    } else {
-        return null;
-    }
-  
-    for (var i = 0; i < filename.length; i++){
-      if (filename[i].RoomName === realRoomName) {
-          filename[i].Display = 'true';
-          console.log(filename)
-          localStorage.setItem(saveto, JSON.stringify(filename)); // Save the updated dataArray back to localStorage
-          window.location.href = window.location.href = filename[i].Path.replace(/"/g, '');
+  var prefix = roomName.slice(0, 2); // extract the first two characters of the search value
+  var floor = roomName.slice(2, 3);
+  var realRoomName = roomName.substring(2);
+  var filename;
+  var saveto;
+  if (prefix === "CA") {
+      if (floor === '1') {
+          //filename = "../../CSV_files/CAMP_first_floor_table.csv";
+           filename = JSON.parse(localStorage.getItem('CAMP1_Array'));
+           saveto = 'CAMP1_Array';
+      } else if (floor === '2') {
+          //filename = "../../CSV_files/CAMP_second_floor_table.csv";
+          filename = JSON.parse(localStorage.getItem('CAMP2_Array'));
+          saveto = 'CAMP2_Array';
+      } else if (floor === '3') {
+          //filename = "../../CSV_files/CAMP_third_floor_table.csv";
+          filename = JSON.parse(localStorage.getItem('CAMP3_Array'));
+          saveto = 'CAMP3_Array'
+      } else{
+          return null;
       }
-    } 
-    return null;
+  } else if (prefix === "SC") {
+      if (floor === '1') {
+          //filename = "../../CSV_files/ScienceCenter_first_floor_table.csv";
+          filename = JSON.parse(localStorage.getItem('SC1_Array'));
+          saveto = 'SC1_Array';
+      } else if (floor === '2') {
+          filename = JSON.parse(localStorage.getItem('SC2_Array'));
+          saveto = 'SC2_Array';
+          //filename = "../../CSV_files/ScienceCenter_second_floor_table.csv";
+      } else if (floor === '3') {
+          filename = JSON.parse(localStorage.getItem('SC3_Array'));
+          saveto = 'SC3_Array';
+          //filename = "../../CSV_files/ScienceCenter_third_floor_table.csv";
+      } else {
+          return null;
+      }
+  } else if (prefix === "SN") {
+      if (floor === 'B') {
+          filename = JSON.parse(localStorage.getItem('SNB_Array'));
+          saveto = 'SNB_Array';
+          //filename = "../../CSV_files/Snell_zero_floor_table.csv";
+      } else if (floor === '1') {
+        filename = JSON.parse(localStorage.getItem('SN1_Array'));
+        saveto = 'SN1_Array';
+          //filename = "../../CSV_files/Snell_first_floor_table.csv";
+      } else if (floor === '2') {
+        filename = JSON.parse(localStorage.getItem('SN2_Array'));
+        saveto = 'SN2_Array';
+          //filename = "../../CSV_files/Snell_second_floor_table.csv";
+      } else if (floor === '3') {
+        filename = JSON.parse(localStorage.getItem('SN3_Array'));
+        saveto = 'SN3_Array';
+          //filename = "../../CSV_files/Snell_third_floor_table.csv";
+      } else {
+          return null;
+      }
+  } else if (prefix === "ST") {
+      if (floor === 'B') {
+        filename = JSON.parse(localStorage.getItem('STB_Array'));
+        saveto = 'STB_Array';
+          //filename = "../../CSV_files/StudentCenter_zero_floor_table.csv";
+      } else if (floor === '1') {
+        filename = JSON.parse(localStorage.getItem('ST1_Array'));
+        saveto = 'ST1_Array';
+          //filename = "../../CSV_files/StudentCenter_first_floor_table.csv";
+      } else if (floor === '2') {
+        filename = JSON.parse(localStorage.getItem('ST2_Array'));
+        saveto = 'ST2_Array';
+          //filename = "../../CSV_files/StudentCenter_second_floor_table.csv";
+      } else {
+          return null;
+      }
+  } else if (prefix === "ER") {
+      if (floor === '1') {
+        filename = JSON.parse(localStorage.getItem('ER1_Array'));
+        saveto = 'ER1_Array';
+          //filename = "../../CSV_files/ERC_first_floor_table.csv";
+      } else if (floor === '2') {
+        filename = JSON.parse(localStorage.getItem('ER2_Array'));
+        saveto = 'ER2_Array';
+          //filename = "../../CSV_files/ERC_second_floor_table.csv";
+      } else {
+          return null;
+      }
+  } else if (prefix === "RO") {
+      if (floor === '1') {
+        filename = JSON.parse(localStorage.getItem('RO1_Array'));
+        saveto = 'RO1_Array';
+          //filename = "../../CSV_files/Rowley_first_floor_table.csv";
+      } else if (floor === '2') {
+        filename = JSON.parse(localStorage.getItem('RO2_Array'));
+        saveto = 'RO2_Array';
+          //filename = "../../CSV_files/Rowley_second_floor_table.csv";
+      } else {
+          return null;
+      }
+  } else {
+      return null;
   }
-  
 
-
+  for (var i = 0; i < filename.length; i++){
+    if (filename[i].RoomName === realRoomName) {
+        filename[i].Display = 'true';
+        console.log(filename)
+        localStorage.setItem(saveto, JSON.stringify(filename)); // Save the updated dataArray back to localStorage
+        window.location.href = window.location.href = filename[i].Path.replace(/"/g, '');
+    }
+  } 
+  return null;
+}
 
 function closeForm() {
     document.getElementById("popupSearch").style.display = "none";
 }
+
 function createHTMLElementAtCoordinatePair(xPercent, yPercent, parentElement) {
   const element = document.createElement('div');
   element.style.position = 'absolute';
-  //element.style.width = `${parentElement.offsetWidth * 0.1}px`;
-  //element.style.height = `${parentElement.offsetHeight * 0.1}px`;
+
   element.style.width = `${10}px`;
   element.style.height = `${10}px`;
   element.style.backgroundColor = 'black';
   parentElement.appendChild(element);
   
-  // Add an event listener to adjust the element's size when the parent element is resized
-  //window.addEventListener('resize', () => {
-  //  element.style.width = `${parentElement.offsetWidth * 0.1}px`;
-  //  element.style.height = `${parentElement.offsetHeight * 0.1}px`;
-  //});
-
   // Get the current dimensions of the parent element
   const parentWidth = parentElement.offsetWidth;
   const parentHeight = parentElement.offsetHeight;
@@ -168,7 +159,7 @@ function createHTMLElementAtCoordinatePair(xPercent, yPercent, parentElement) {
 }
 function searchAndCreate1(element_ID) {
     // Retrieve the array from localStorage
-    var dataArray = JSON.parse(localStorage.getItem('ER1_Array'));
+    var dataArray = JSON.parse(localStorage.getItem('CAMP1_Array'));
     console.log(dataArray[1])
     // Check if dataArray is not null or undefined
     if (dataArray) {
@@ -186,9 +177,9 @@ function searchAndCreate1(element_ID) {
       console.log('myDataArray is null or undefined. Please load CSV data first.');
     }
 }
-function searchAndCreate2(element_ID) {
+  function searchAndCreate2(element_ID) {
     // Retrieve the array from localStorage
-    var dataArray = JSON.parse(localStorage.getItem('ER2_Array'));
+    var dataArray = JSON.parse(localStorage.getItem('CAMP2_Array'));
     console.log(dataArray[1])
     // Check if dataArray is not null or undefined
     if (dataArray) {
@@ -205,34 +196,80 @@ function searchAndCreate2(element_ID) {
     } else {
       console.log('myDataArray is null or undefined. Please load CSV data first.');
     }
-}
-function makefalse1() {
-
-  var dataArray = JSON.parse(localStorage.getItem('ER1_Array'));// Retrieve the array from localStorage
-  
-  if (dataArray) { // Check if dataArray is not null or undefined
-
-    for (var i = 0; i < dataArray.length; i++) {// Loop through the array and set the Display property to true
-     dataArray[i].Display = 'false';
-    }
-
-    localStorage.setItem('ER1_Array', JSON.stringify(dataArray)); // Save the updated dataArray back to localStorage
-  } else {
-    console.log('ER1_Array is null or undefined. Please load CSV data first.');
   }
-}
-function makefalse2() {
-
-  var dataArray = JSON.parse(localStorage.getItem('ER2_Array'));// Retrieve the array from localStorage
-  
-  if (dataArray) { // Check if dataArray is not null or undefined
-
-    for (var i = 0; i < dataArray.length; i++) {// Loop through the array and set the Display property to true
-     dataArray[i].Display = 'false';
+function searchAndCreate3(element_ID) {
+    // Retrieve the array from localStorage
+    var dataArray = JSON.parse(localStorage.getItem('CAMP3_Array'));
+    console.log(dataArray[1])
+    // Check if dataArray is not null or undefined
+    if (dataArray) {
+      // Loop through the array and call createHTMLElementAtCoordinatePair for elements with Display set to true
+      for (var i = 0; i < dataArray.length; i++) {
+        var displayValue = dataArray[i].Display.trim().toLowerCase(); // Convert to lowercase and remove leading/trailing spaces
+        if (displayValue === 'true' || displayValue === 'true\r') {
+            var x = parseInt(dataArray[i].XCoord);
+            var y = parseInt(dataArray[i].YCoord);
+            console.log(dataArray[i].RoomName)
+          createHTMLElementAtCoordinatePair(x, y,element_ID);
+        }
+      }
+    } else {
+      console.log('myDataArray is null or undefined. Please load CSV data first.');
     }
-
-    localStorage.setItem('ER2_Array', JSON.stringify(dataArray)); // Save the updated dataArray back to localStorage
-  } else {
-    console.log('ER2_Array is null or undefined. Please load CSV data first.');
   }
-}
+  function makefalse1() {
+
+    var dataArray = JSON.parse(localStorage.getItem('ScienceCenter1_Array'));// Retrieve the array from localStorage
+    
+    if (dataArray) { // Check if dataArray is not null or undefined
+
+      for (var i = 0; i < dataArray.length; i++) {// Loop through the array and set the Display property to true
+       dataArray[i].Display = 'false';
+      }
+  
+      localStorage.setItem('ScienceCenter1_Array', JSON.stringify(dataArray)); // Save the updated dataArray back to localStorage
+    } else {
+      console.log('ScienceCenter1_Array is null or undefined. Please load CSV data first.');
+    }
+  }
+  function makefalse2() {
+
+    var dataArray = JSON.parse(localStorage.getItem('ScienceCenter2_Array'));// Retrieve the array from localStorage
+    
+    if (dataArray) { // Check if dataArray is not null or undefined
+
+      for (var i = 0; i < dataArray.length; i++) {// Loop through the array and set the Display property to true
+       dataArray[i].Display = 'false';
+      }
+  
+      localStorage.setItem('ScienceCenter2_Array', JSON.stringify(dataArray)); // Save the updated dataArray back to localStorage
+    } else {
+      console.log('ScienceCenter2_Array is null or undefined. Please load CSV data first.');
+    }
+  }
+  function makefalse3() {
+
+    var dataArray = JSON.parse(localStorage.getItem('ScienceCenter3_Array'));// Retrieve the array from localStorage
+    
+    if (dataArray) { // Check if dataArray is not null or undefined
+
+      for (var i = 0; i < dataArray.length; i++) {// Loop through the array and set the Display property to true
+       dataArray[i].Display = 'false';
+      }
+  
+      localStorage.setItem('ScienceCenter3_Array', JSON.stringify(dataArray)); // Save the updated dataArray back to localStorage
+    } else {
+      console.log('ScienceCenter3_Array is null or undefined. Please load CSV data first.');
+    }
+  }
+
+
+
+
+
+
+
+
+
+
+
