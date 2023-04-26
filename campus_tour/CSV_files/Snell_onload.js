c1972c60dcbed1aa20d345eda8516a9a28221515
polarolo <115050592+polarolo@users.noreply.github.com>
2023-04-25 22:48:32 -0400

    snell update

---
+++ campus_tour/CSV_files/Snell_onload.js
@@ -0,0 +1,120 @@
+function SnellB_CSV_to_Array() {
+    var xhr = new XMLHttpRequest();// Create a new XMLHttpRequest object
+
+    xhr.onload = function () {// Set the onload callback function
+        if (xhr.status === 200) {
+            var lines = xhr.responseText.split('\n');// Parse the CSV data
+            var dataArray = [];
+            for (var i = 0; i < lines.length; i++) {
+                var values = lines[i].split(',');
+                var obj = {
+                    RoomName: values[0],
+                    XCoord: values[1],
+                    YCoord: values[2],
+                    Path: values[3],
+                    Display: values[4]
+                };
+                dataArray.push(obj);
+            }
+
+            localStorage.setItem('SnellB_Array', JSON.stringify(dataArray));// Store the array in localStorage
+
+            console.log('SnellB First element:', dataArray[1]);
+        }
+    };
+
+    var file = './campus_tour/CSV_files/Snell_zero_floor_table.csv';  // Set the default file path
+
+    xhr.open('GET', file, true);// Open the file and send the request
+    xhr.send();
+}
+function Snell1_CSV_to_Array() {
+    var xhr = new XMLHttpRequest();// Create a new XMLHttpRequest object
+
+    xhr.onload = function () {// Set the onload callback function
+        if (xhr.status === 200) {
+            var lines = xhr.responseText.split('\n');// Parse the CSV data
+            var dataArray = [];
+            for (var i = 0; i < lines.length; i++) {
+                var values = lines[i].split(',');
+                var obj = {
+                    RoomName: values[0],
+                    XCoord: values[1],
+                    YCoord: values[2],
+                    Path: values[3],
+                    Display: values[4]
+                };
+                dataArray.push(obj);
+            }
+
+            localStorage.setItem('Snell1_Array', JSON.stringify(dataArray));// Store the array in localStorage
+
+            console.log('Snell1 First element:', dataArray[1]);
+        }
+    };
+
+    var file = './campus_tour/CSV_files/Snell_first_floor_table.csv';  // Set the default file path
+
+    xhr.open('GET', file, true);// Open the file and send the request
+    xhr.send();
+}
+function Snell2_CSV_to_Array() {
+    var xhr = new XMLHttpRequest();// Create a new XMLHttpRequest object
+
+    xhr.onload = function () {// Set the onload callback function
+        if (xhr.status === 200) {
+            var lines = xhr.responseText.split('\n');// Parse the CSV data
+            var dataArray = [];
+            for (var i = 0; i < lines.length; i++) {
+                var values = lines[i].split(',');
+                var obj = {
+                    RoomName: values[0],
+                    XCoord: values[1],
+                    YCoord: values[2],
+                    Path: values[3],
+                    Display: values[4]
+                };
+                dataArray.push(obj);
+            }
+
+            localStorage.setItem('Snell2_Array', JSON.stringify(dataArray));// Store the array in localStorage
+
+            console.log('Snell2 First element:', dataArray[1]);
+        }
+    };
+
+    var file = './campus_tour/CSV_files/Snell_second_floor_table.csv';  // Set the default file path
+
+    xhr.open('GET', file, true);// Open the file and send the request
+    xhr.send();
+}
+function Snell3_CSV_to_Array() {
+    var xhr = new XMLHttpRequest();// Create a new XMLHttpRequest object
+
+    xhr.onload = function () {// Set the onload callback function
+        if (xhr.status === 200) {
+            var lines = xhr.responseText.split('\n');// Parse the CSV data
+            var dataArray = [];
+            for (var i = 0; i < lines.length; i++) {
+                var values = lines[i].split(',');
+                var obj = {
+                    RoomName: values[0],
+                    XCoord: values[1],
+                    YCoord: values[2],
+                    Path: values[3],
+                    Display: values[4]
+                };
+                dataArray.push(obj);
+            }
+
+            localStorage.setItem('Snell3_Array', JSON.stringify(dataArray));// Store the array in localStorage
+
+            console.log('Snell3 First element:', dataArray[1]);
+        }
+    };
+
+    var file = './campus_tour/CSV_files/Snell_third_floor_table.csv';  // Set the default file path
+
+    xhr.open('GET', file, true);// Open the file and send the request
+    xhr.send();
+}
